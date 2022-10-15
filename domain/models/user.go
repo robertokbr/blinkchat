02259f4ddb6972bdbf7d89d629609fc656b74b5a
robkbr <roberto.junior@nickelpay.com.br>
2022-10-15 14:17:30 -0300

    feat: add new user function

--- domain/models/user.go
+++ domain/models/user.go
@@ -1,13 +1,35 @@
 package models
 
-import "github.com/robertokbr/blinkchat/domain/enums"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/robertokbr/blinkchat/domain/enums"
+)
 
 type User struct {
-	ID        int             `json:"id"`
-	Name      string          `json:"name"`
-	ImageURL  string          `json:"image_url"`
-	Email     string          `json:"email"`
-	State     enums.UserState `json:"state"`
-	CreatedAt string          `json:"created_at"`
-	UpdatedAt string          `json:"updated_at"`
+	ID           string          `json:"id"`
+	ConnectionID string          `json:"connection_id"`
+	Name         string          `json:"name"`
+	ImageURL     string          `json:"image_url"`
+	Email        string          `json:"email"`
+	State        enums.UserState `json:"state"`
+	CreatedAt    time.Time       `json:"created_at"`
+	UpdatedAt    time.Time       `json:"updated_at"`
+}
+
+// TODO: Add validation
+func NewUser(connectionID, name, imageURL, email string, state enums.UserState, createdAt, updatedAt time.Time) *User {
+	id := uuid.NewString()
+
+	return &User{
+		ID:           id,
+		ConnectionID: connectionID,
+		Name:         name,
+		ImageURL:     imageURL,
+		Email:        email,
+		State:        state,
+		CreatedAt:    createdAt,
+		UpdatedAt:    updatedAt,
+	}
 }
