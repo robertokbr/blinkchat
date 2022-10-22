4d4a5fc741207c5405f2c4181447481fc0423cf5
robkbr <roberto.junior@nickelpay.com.br>
2022-10-22 14:58:36 -0300

    feat: add websocket connections controller

---
+++ infrastructure/controllers/websocket_connections.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/robertokbr/blinkchat/domain/dtos"
+	"github.com/robertokbr/blinkchat/domain/models"
+	"github.com/robertokbr/blinkchat/infrastructure/pkg/websocket"
+)
+
+type WebsocketConnections struct {
+	Pool *websocket.Pool
+}
+
+func (wsc *WebsocketConnections) Create(w http.ResponseWriter, r *http.Request) {
+	connection, err := websocket.Upgrade(r, w)
+
+	if err != nil {
+		log.Printf("error upgrading connection: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not upgrade connection"))
+		return
+	}
+
+	var createConnectionDTO dtos.CreateConnection
+
+	query := r.URL.Query()
+
+	createUserString := query.Get("user")
+
+	json.Unmarshal([]byte(createUserString), &createConnectionDTO)
+
+	log.Printf("Connecting user: %v", createConnectionDTO)
+
+	user := models.NewUser(createConnectionDTO)
+
+	client := &websocket.Client{
+		User: user,
+		Conn: connection,
+		Pool: wsc.Pool,
+	}
+
+	client.Pool.Register <- client
+
+	client.Read()
+}
+
+func (wsc *WebsocketConnections) FindAll(w http.ResponseWriter, r *http.Request) {
+	connections := make([]*models.User, 0)
+
+	for client := range wsc.Pool.Clients {
+		connections = append(connections, client.User)
+	}
+
+	serialized, err := json.Marshal(connections)
+
+	if err != nil {
+		log.Printf("error serializing connections: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not serialize connections"))
+		return
+	}
+
+	w.Write(serialized)
+}
