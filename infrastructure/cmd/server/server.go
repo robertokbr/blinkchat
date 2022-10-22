0b3eda251c5ced6cc9058ef85757ad40d7fc1e84
robkbr <roberto.junior@nickelpay.com.br>
2022-10-22 15:13:08 -0300

    feat: start multiple pool handlers

--- infrastructure/cmd/server/server.go
+++ infrastructure/cmd/server/server.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/robertokbr/blinkchat/infrastructure/controllers"
+	"github.com/robertokbr/blinkchat/infrastructure/pkg/websocket"
+)
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
+
 func main() {
-	println("Hello, World!")
+	var pool = websocket.NewPool()
+
+	var websocketConnectionsController = controllers.WebsocketConnections{Pool: pool}
+
+	for i := 0; i < 10; i++ {
+		go pool.Start(i)
+	}
+
+	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/ws", websocketConnectionsController.Create)
+	http.HandleFunc("/connections", websocketConnectionsController.FindAll)
+
+	log.Println("Server started on port 8080")
+
+	http.ListenAndServe(":8080", nil)
 }
