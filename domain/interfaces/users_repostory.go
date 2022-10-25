19388a31efa174312e9ceea1b35f37243182a92b
robkbr <roberto.junior@nickelpay.com.br>
2022-10-25 20:31:42 -0300

    chore: remove unused function

--- domain/interfaces/users_repostory.go
+++ domain/interfaces/users_repostory.go
@@ -1,12 +1,10 @@
 package interfaces
 
 import (
-	"github.com/robertokbr/blinkchat/domain/dtos"
 	"github.com/robertokbr/blinkchat/domain/models"
 )
 
 type UsersRepository interface {
-	Create(data dtos.CreateConnection) (*models.User, error)
 	FindByEmail(email string) (*models.User, error)
 	Save(user *models.User) error
 }
