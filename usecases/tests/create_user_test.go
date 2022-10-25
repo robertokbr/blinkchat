6005bce39f434216589e91419c6c6f461e8b59b1
robkbr <roberto.junior@nickelpay.com.br>
2022-10-25 20:29:41 -0300

    test: add create user test

---
+++ usecases/tests/create_user_test.go
@@ -0,0 +1,62 @@
+package usecase_tests
+
+import (
+	"testing"
+
+	"github.com/robertokbr/blinkchat/domain/dtos"
+	"github.com/robertokbr/blinkchat/domain/models"
+	"github.com/robertokbr/blinkchat/infrastructure/database"
+	"github.com/robertokbr/blinkchat/infrastructure/database/repositories"
+	"github.com/robertokbr/blinkchat/usecases"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUser(t *testing.T) {
+	db := database.NewDatabase()
+
+	connection, err := db.Connect()
+
+	require.Nil(t, err)
+
+	usersRepository := repositories.NewUsersRepository(connection)
+
+	createUser := usecases.NewCreateUser(usersRepository)
+
+	createUserDTO := dtos.CreateConnection{
+		Email:    "email@email.com",
+		Name:     "name",
+		ImageURL: "image_url",
+	}
+
+	createdUser, err := createUser.Execute(createUserDTO)
+
+	require.Nil(t, err)
+
+	require.Equal(t, createUserDTO.Email, createdUser.Email)
+}
+
+func TestReturnAlreadyCreatedUser(t *testing.T) {
+	db := database.NewDatabase()
+	connection, err := db.Connect()
+
+	require.Nil(t, err)
+
+	usersRepository := repositories.NewUsersRepository(connection)
+	createUser := usecases.NewCreateUser(usersRepository)
+
+	createUserDTO := dtos.CreateConnection{
+		Email:    "email@email.com",
+		Name:     "name",
+		ImageURL: "image_url",
+	}
+
+	user := models.NewUser(createUserDTO)
+
+	usersRepository.Save(user)
+
+	createdUser, err := createUser.Execute(createUserDTO)
+
+	require.Nil(t, err)
+
+	require.Equal(t, user.Email, createdUser.Email)
+}
