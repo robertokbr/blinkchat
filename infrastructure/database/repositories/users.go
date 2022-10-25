6be1e16d31cf1fcb547fe6f9919ac8b50b27051f
robkbr <roberto.junior@nickelpay.com.br>
2022-10-25 20:31:09 -0300

    feat: add users repository implementation

---
+++ infrastructure/database/repositories/users.go
@@ -0,0 +1,40 @@
+package repositories
+
+import (
+	"github.com/robertokbr/blinkchat/domain/models"
+	"gorm.io/gorm"
+)
+
+type Users struct {
+	db *gorm.DB
+}
+
+func NewUsersRepository(db *gorm.DB) *Users {
+	return &Users{db: db}
+}
+
+func (u *Users) FindByEmail(email string) (*models.User, error) {
+	user := &models.User{}
+
+	err := u.db.Where("email = ?", email).First(user).Error
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *Users) Save(user *models.User) error {
+	err := u.db.Save(user).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
