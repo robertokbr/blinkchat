b219dd6ce211607d9227c17a1131de24bb114170
robertokbr <robertojuniordev@gmail.com>
2023-03-07 14:19:50 -0300

    feat: add spreadmessage, checkIfClientIsOnline functions and change clients map to map client id to client

--- src/infrastructure/pkg/websocket/pool.go
+++ src/infrastructure/pkg/websocket/pool.go
@@ -11,8 +11,9 @@ import (
 type Pool struct {
 	Register   chan *Client
 	Unregister chan *Client
-	Clients    map[*Client]bool
+	Clients    map[string]*Client
 	Broadcast  chan models.Message
+	Match      chan models.Message
 	CreatedAt  time.Time
 }
 
@@ -20,10 +21,24 @@ func NewPool() *Pool {
 	return &Pool{
 		Register:   make(chan *Client),
 		Unregister: make(chan *Client),
-		Clients:    make(map[*Client]bool),
+		Clients:    make(map[string]*Client),
 		Broadcast:  make(chan models.Message),
+		Match:      make(chan models.Message),
 		CreatedAt:  time.Now(),
 	}
+}
+
+func (pool *Pool) SpreadMessage(message models.Message) {
+	switch message.Action {
+	case enums.BROADCASTING:
+		pool.Broadcast <- message
+	case enums.MATCHING:
+		pool.Match <- message
+	}
+}
+
+func (pool *Pool) CheckIfClientIsOnline(client *Client) bool {
+	return pool.Clients[client.ID] != nil
 }
 
 func (pool *Pool) Start(poolNumber int) {
@@ -34,7 +49,7 @@ func (pool *Pool) Start(poolNumber int) {
 		case client := <-pool.Register:
 			log.Printf("[Pool %v]: Registering client %v", poolNumber, client.User.ID)
 
-			pool.Clients[client] = true
+			pool.Clients[client.ID] = client
 
 			message := models.NewMessage(
 				"New user joined",
@@ -44,8 +59,8 @@ func (pool *Pool) Start(poolNumber int) {
 			)
 
 			go func() {
-				for client := range pool.Clients {
-					client.Conn.WriteJSON(message)
+				for _, pc := range pool.Clients {
+					pc.Conn.WriteJSON(message)
 				}
 			}()
 
@@ -53,7 +68,7 @@ func (pool *Pool) Start(poolNumber int) {
 		case client := <-pool.Unregister:
 			log.Printf("[Pool %v]: Unregistering client %v", poolNumber, client.User.ID)
 
-			delete(pool.Clients, client)
+			delete(pool.Clients, client.ID)
 
 			message := models.NewMessage(
 				"User has disconnected",
@@ -63,19 +78,26 @@ func (pool *Pool) Start(poolNumber int) {
 			)
 
 			go func() {
-				for client := range pool.Clients {
-					client.Conn.WriteJSON(message)
+				if client.Pair != nil {
+					// Unmatch the pair
+					client.Pair.Unmatch()
+				}
+
+				for _, pc := range pool.Clients {
+					pc.Conn.WriteJSON(message)
 				}
 			}()
 
 			break
 		case message := <-pool.Broadcast:
-			for client := range pool.Clients {
+			for _, client := range pool.Clients {
 				if err := client.Conn.WriteJSON(message); err != nil {
 					log.Printf("[Pool %v]: error writing message: %v", poolNumber, err)
 					return
 				}
 			}
+
+			break
 		}
 	}
 }
