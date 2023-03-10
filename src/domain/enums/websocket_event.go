d852108a5b4fd0267708b1a902edcb37b73bae9c
robertokbr <robertojuniordev@gmail.com>
2023-03-10 17:29:10 -0300

    chore: remove unused enum from WebsocketEvent

--- src/domain/enums/websocket_event.go
+++ src/domain/enums/websocket_event.go
@@ -5,7 +5,6 @@ type WebsocketEvent string
 const (
 	DISCONNECTED WebsocketEvent = "disconnected"
 	CONNECTED    WebsocketEvent = "connected"
-	SEARCHING    WebsocketEvent = "searching"
 	MATCHING     WebsocketEvent = "matching"
 	UNMATCHING   WebsocketEvent = "unmatching"
 	BROADCASTING WebsocketEvent = "broadcasting"
