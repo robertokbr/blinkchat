0f494512d620417cc3f0f8a61d3d2b5e1afcf23b
robertokbr <robertojuniordev@gmail.com>
2023-03-22 13:52:12 -0300

    chore: update enums

--- src/domain/enums/websocket_event.go
+++ src/domain/enums/websocket_event.go
@@ -7,5 +7,7 @@ const (
 	CONNECTED    WebsocketEvent = "connected"
 	MATCHING     WebsocketEvent = "matching"
 	UNMATCHING   WebsocketEvent = "unmatching"
+	MATCHED      WebsocketEvent = "matched"
+	UNMATCHED    WebsocketEvent = "unmatched"
 	BROADCASTING WebsocketEvent = "broadcasting"
 )
