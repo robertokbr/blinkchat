7345ffe38fdc3f43e0f399dcefdddd242e61252a
robertokbr <robertojuniordev@gmail.com>
2023-04-17 17:58:49 -0300

    feat: add websocket upgrader

---
+++ src/infrastructure/websocket/websocket.go
@@ -0,0 +1,24 @@
+package websocket
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/robertokbr/blinkchat/src/infrastructure/middlewares"
+)
+
+func Upgrade(r *http.Request, w http.ResponseWriter) (*websocket.Conn, error) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin:     middlewares.Cors,
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		return &websocket.Conn{}, err
+	}
+
+	return conn, nil
+}
