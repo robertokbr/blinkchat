29751701979ab9d8d95cd683102362c10f7429e2
robertokbr <robertojuniordev@gmail.com>
2023-04-17 17:59:22 -0300

    feat: update session model

--- src/domain/models/session.go
+++ src/domain/models/session.go
@@ -7,16 +7,17 @@ import (
 )
 
 type Session struct {
-	ID        string `json:"id"`
-	Code      string `json:"code"`
-	UserID    string `json:"user_id"`
+	ID    string `json:"id" gorm:"primaryKey ;type:uuid;"`
+	Token string `json:"token" gorm:"type:uuid;"`
+	// references the user table
+	UserID    string `json:"user_id" gorm:"type:uuid; references:users(id); on delete cascade;"`
 	CreatedAt int64  `json:"expires_at"`
 }
 
 func NewSession(userID string) *Session {
 	return &Session{
 		ID:        uuid.NewString(),
-		Code:      uuid.NewString(),
+		Token:     uuid.NewString(),
 		UserID:    userID,
 		CreatedAt: time.Now().UnixMilli(),
 	}
