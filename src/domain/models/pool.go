a06e6e7b8dd70c44441bb912b39afb8ae636643a
robertokbr <robertojuniordev@gmail.com>
2023-04-27 13:54:34 -0300

    refactor: remove concerns from pool model

--- src/domain/models/pool.go
+++ src/domain/models/pool.go
@@ -1,42 +1,15 @@
 package models
 
-import (
-	"github.com/robertokbr/blinkchat/src/domain/enums"
-	"github.com/robertokbr/blinkchat/src/domain/logger"
-)
-
 type Pool struct {
-	Broadcast chan Message
-	Match     chan Message
-	Unmatch   chan Message
-	Clients   map[string]*Client
-	Pairs     []*Client
+	Clients map[string]*Client
+	Pairs   chan *Client
 }
 
 func NewPool() *Pool {
 	pool := Pool{
-		Broadcast: make(chan Message),
-		Match:     make(chan Message),
-		Unmatch:   make(chan Message),
-		Clients:   make(map[string]*Client),
-		Pairs:     make([]*Client, 0),
+		Clients: make(map[string]*Client),
+		Pairs:   make(chan *Client),
 	}
 
 	return &pool
-}
-
-func (p *Pool) PushMessage(message Message) {
-	switch message.Action {
-	case enums.BROADCASTING:
-		p.Broadcast <- message
-		break
-	case enums.MATCHING:
-		p.Match <- message
-		break
-	case enums.UNMATCHING:
-		p.Unmatch <- message
-		break
-	default:
-		logger.Debugf("Receiving message %v", message)
-	}
 }
